50c0cb1aa9279af508ea1306e0d04e066bd895a2
Alan Llamas <allamas@frba.utn.edu.ar>
2026-04-05 20:15:14 -0300

    Se sube el archivo fuente y el readme

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Hello, World! \n");
+    printf("la version del compildaor es: %d \n", __STDC_VERSION__);
+
+    return 0;
+}
---
+++ 00-CHelloWorld/readme.md
@@ -0,0 +1,5 @@
+#Información técnica
+
+* -Compilador seleccionado: GCC (vía MinGW-w64/MSYS2)
+* -Versión del compilador: 15.2.0
+* -Versión de C: c23 (202311)
